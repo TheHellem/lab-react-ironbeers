08477625e3df7aceb8f42fc39c82271c68787539
HenrikH <henrik.hellem@live.com>
2023-02-24 15:17:07 +0100

    Iteration 1 (minimal styling)

---
+++ src/pages/Home.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+import beer from "../assets/beers.png";
+import newBeer from "../assets/new-beer.png";
+import randomBeer from "../assets/random-beer.png";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const Home = () => {
+  return (
+    <Container fluid="md">
+      <Row>
+        <Col lg={true}>
+          <section className="">
+            <img src={beer} alt="beer" />
+            <h1>All Beers</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua. At vero eos et accusam et justo duo
+              dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+              sanctus est Lorem ipsum dolor sit amet.
+            </p>
+          </section>
+          <section>
+            <img src={randomBeer} alt="beer" />
+            <h1>Random Beer</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua. At vero eos et accusam et justo duo
+              dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+              sanctus est Lorem ipsum dolor sit amet.
+            </p>
+          </section>
+          <section>
+            <img src={newBeer} alt="beer" />
+            <h1>New Beer</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua. At vero eos et accusam et justo duo
+              dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+              sanctus est Lorem ipsum dolor sit amet.
+            </p>
+          </section>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Home;
